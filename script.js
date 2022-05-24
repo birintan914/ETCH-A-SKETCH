15dd65746c75ef71a3f8c0a9d77b31ec54a4332b
Birintan <birintancs@gmail.com>
2022-05-24 07:40:40 -0400

    Finish creating a grid with js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let body = document.body;
 
 for(let i=0; i<16; i++){
-    let col = document.createElement('div')
-    col.classList.add("col")
-    for(let x=0; x<3; x++){
-        let row = document.createElement('div')
-        row.classList.add('row');
-        col.appendChild(row);
+    let row = document.createElement('div')
+    row.classList.add("row")
+    for(let x=0; x<16; x++){
+        let box = document.createElement('div')
+        box.classList.add("box")
+        row.appendChild(box)
     }
-    body.appendChild(col)
+    body.appendChild(row)
 }
 
