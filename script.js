c2ceab59467e357d4f5f7fadc2c00fd355de03c5
Birintan <birintancs@gmail.com>
2022-05-24 07:32:10 -0400

    Rework code to display multiple rows using js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let body = document.body;
 
-for(let i=0; i<10; i++){
-    let box = document.createElement('div')
-    box.classList.add("box")
-    body.appendChild(box)
+for(let i=0; i<16; i++){
+    let col = document.createElement('div')
+    col.classList.add("col")
+    for(let x=0; x<3; x++){
+        let row = document.createElement('div')
+        row.classList.add('row');
+        col.appendChild(row);
+    }
+    body.appendChild(col)
 }
 
